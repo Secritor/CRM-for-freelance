fdc1335b13aab0c264a838f56a1336183056402f
Алексей Макаров <raminov.83@mail.ru>
2026-03-11 16:40:51 +0300

    пофикшена  проблема с путями до минифицированных файлов в билде из-за неправильной настройки конфига vite.config.ts, из-за чего билд не работал корректно

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/CRM-for-freelance/' : '/',
+  base: command === 'build' ? './' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
